21f92d3aa083268c9da5f819bf2cecef03e39182
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2021-08-18 17:24:35 +0100

    Further refactoring. added sacstats instead of using multiple long calls to a trace stats.sac Dict. Updated (an renamed) measure_splitting method to use the updated subprocess.run() function insted of POpen. Re-organised ored of functions and tidying up path requirements. the rundir path is now an optional argument when creating the class, if no path is specified we default to the cwd.

--- wrapper.py
+++ wrapper.py
@@ -20,84 +20,28 @@ class Interface:
     Class which will act as the interface to sheba.
     The "subprocess" sheba will be a bound method
     """
-    def __init__(self,st):
+    def __init__(self,st, phase, rundir=None):
         
         self.st = st
+        self.sacstats = st[0].stats.sac
+        self.station = st[0].stats.station # station code 
+        self.delta = st[0].stats.delta # sample rate of seismometer [s]
+        # do some basic tests of the data 
+        self.check_phase_dist(phase)
+        self.phase = phase # The shear-wave phase we are measuing splitting for!
         for trace in self.st:
             trace.stats.sac.kstnm = '{:>8}'.format(trace.stats.sac.kstnm)
 #          Formats Station name in headers so that it is 8 characters long, with emtpy character fill with whitespaces    
             self.check_evdp(trace)
-                
-        self.gcarc = st[0].stats.sac.gcarc # source-reciever great circle distance [deg]
-        self.station = st[0].stats.station # station code 
-        self.delta = st[0].stats.delta # sample rate of seismometer [s]
         
-    def check_evdp(self, trace):
-        '''
-        Tests the event depths 
-        '''
-        evdp = trace.stats.sac.evdp
-        if trace.stats.sac.evdp >= 1000:
-            raise Warning('Event depth is greater than 1000km! EVDP may be in meters')
-            trace.stats.sac({'evdp':evdp/1000})
-        elif trace.stats.sac.evdp == 0:
-            raise ValueError('Event depth is 0km!')
-        
-    def model_traveltimes(self,phase):
-        """
-        Function to run TauP traveltime models for the SKS phase.
-        Returns SKS predictided arrivals (seconds), origin time of the event (t0) as a UTCDateTime obejct and the SKS arrival as a UTCDateTime object
-        tr - trace object for which SKS arrival time will be predicted
-        """
-        model = TauPyModel(model="iasp91")
-        tt = model.get_travel_times((self.horz1[0].stats.sac.evdp),
-                                     self.horz1[0].stats.sac.gcarc,
-                                     [phase])
-        try:
-            traveltime = tt[0].time
-        except IndexError:
-            print('index Error')
-            traveltime = None
-
-        evt_time = obspy.UTCDateTime(year = self.horz1[0].stats.sac.nzyear,
-                                     julday = self.horz1[0].stats.sac.nzjday,
-                                     hour=self.horz1[0].stats.sac.nzhour,
-                                     minute=self.horz1[0].stats.sac.nzmin,
-                                     second=self.horz1[0].stats.sac.nzsec,
-                                     microsecond=self.horz1[0].stats.sac.nzmsec)
-        self.tt = evt_time + traveltime
-        self.tt_rel = traveltime
-        return traveltime
-
-    def check_phase_dist(self,phase_to_check):
-        """
-        Function to test if the given phase is actually measureable!
-        """
-        if phase_to_check == 'SKS':
-            if self.gcarc > 145.0:
-                raise ValueError('Event-Station distance is greater than 145, SKS not visible/reliable.')
-        elif phase_to_check == 'SKKS':
-            if (self.gcarc < 105.0) or (self.gcarc > 145.0) :
-                raise ValueError('Event-Station distance outside of acceptable range of 105-145 for SKKS')
-        elif phase_to_check == 'ScS':
-            if self.gcarc > 95.0:
-                raise ValueError('Event-Station distance greatr than acceptable distance of 95 deg for ScS')
+        self.tt_utc, self.tt_rel = self.model_traveltimes()
+        if rundir is None:
+            print('Setting rundir path to current working directory')
+            self.path = os.getcwd()
         else:
-            print('Phase {} not ScS, SKS or SKKS. Not checking!'.format(phase_to_check))
-
-    def initialise_windows(self, trace):
-        if all (k in trace.stats.sac for k in ('user0','user1','user2','user3')):
-            print('Pre-defined window ranges found')
-        else:
-            user0 = self.tt_rel - 15 # 15 seconds before arrival
-            user1 = self.tt_rel # t predicted arrival
-#           Set the range of window endtime (user2/user3)
-            user2 = self.tt_rel + 15 # 15 seconds after, gives a min window size of 20 seconds
-            user3 = self.tt_rel + 30 # 30 seconds after, gives a max window size of 45 seconds
-            keychain = {'user0':user0,'user1':user1,'user2':user2,'user3':user3}
-            trace.stats.sac.update(keychain)
-
-    def preprocess(self,synth=False,c1=0.01,c2=0.5):
+            self.path = rundir
+    
+    def preprocess(self,c1=0.01,c2=0.5):
         """
         Function to bandpass filter and trim the components
         Seismograms are trimmed so that they start 1 minute before the expected arrival and end 2 minutes after the arrival
@@ -114,20 +58,55 @@ class Interface:
             trace.filter("bandpass",freqmin= c1, freqmax= c2,corners=2,zerophase=True)
 #       Now trim each component to the input length
 #       We only need to trim and set the window length for real data, not synthetics made with sacsplitwave
-            if synth == False: 
-#               Now set the trim
-                print('Trim Traces')
-                t1 = (self.tt - 60) #I.e A minute before the arrival
-                t2 = (self.tt+ 120) #I.e Two minutes after the arrival
-                trace.trim(t1,t2)
+#           Now set the trim
+            print('Trim Traces')
+            t1 = (self.tt_utc - 60) #I.e A minute before the arrival
+            t2 = (self.tt_utc + 120) #I.e Two minutes after the arrival
+            trace.trim(t1,t2)
 #               Initialise Windows        
-                self.initialise_windows(trace)
+            self.initialise_windows(trace)
+
+    def measure_splitting(self,output_filename, sheba_exec_path):
+        """
+        Measures Shear-wave splitting using Sheba. 
+        """
+        fname = f'{output_filename}_{self.phase}'
+        self.gen_infile(fname)
+        self.write_out(fname)
+        print(f'Worker {current_process()} Passing {label} into Sheba.')
+        out = sub.run(f'{sheba_exec_path}/sheba_exec', capture_output=True, cwd=self.rundir)
+
+    def gen_infile(self,filename, nwind=10, tlag_max=4.0):
+        '''
+        Generates the input file needed for the Fortran SHEBA routines.
+        '''
+        with open(f'{self.path}/sheba.in','w') as writer:
+            writer.write('SHEBA.IN \n')
+            writer.write(f'{filename} \n') # Name of pre-processed data 
+            for trace in self.st:
+                writer.write('{} \n'.format(trace.stats.channel)) # Write channels for each component (E, N, Z order)
+            writer.write('1 \n') # Specifies Eigenvalue minimisation, replace with spol if transverse minimisation is desired (not supported here)
+            writer.write('{} {} \n'.format(nwind,nwind))
+            writer.write('{} \n'.format(tlag_max)) # sets max tlag in gridsearch
+            writer.write('0')  
+            writer.write('0')
+   
+    def write_out(self,filename):
+      """
+      Function to write the component seismograms to SAC files to the SHEBA rundir.
+      The rundir is set by self.path.
+      
+      filename [str] - the output filename for each trace. 
+      
+      """
+      for trace in self.st:
+          ch = trace.stats.channel
+          trace.write(f'{self.path}/{filename}.{ch}', format='SAC', byteorder=1)
 
     def window_event(self):
         '''Function that enables manual picking of window start/end ranges'''
         # Windowing code
-        st = self.horz1 + self.horz2
-        Windower = WindowPicker(st,
+        Windower = WindowPicker(self.st,
                                 self.st[0].stats.sac['user0'], self.st[0].stats.sac['user1'],
                                 self.st[0].stats.sac['user2'], self.st[0].stats.sac['user3'],
                                 self.tt_rel)
@@ -140,67 +119,63 @@ class Interface:
                       'user2' : Windower.wend1, 'user3' : Windower.wend2}
             [trace.stats.sac.update(windows) for trace in self.st]
             return True
-      
-    def write_out(self,phase,label,path=None):
+        
+    def model_traveltimes(self):
         """
-        Function to write the component seismograms to SAC files within the sheba directory structure so Sheba can access them
-        station [str] - station code
-        phase [str] - phase code for which seismic phase splitting is being meausured
-        path [str] - path that you want the seismogrmas saved to.
+        Function to run TauP traveltime models for the SKS phase.
+        Returns SKS predictided arrivals (seconds), origin time of the event (t0) as a UTCDateTime obejct and the SKS arrival as a UTCDateTime object
+        tr - trace object for which SKS arrival time will be predicted
         """
-        for trace in self.st:
-            ch = trace.stats.channel
-            if path is None:
-                print(f'Writing out to CWD: {path}')
-                path = os.get_cwd()
-                
-            trace.write(f'{path}/{label}{phase}.{ch}', format='SAC', byteorder=1)
+        model = TauPyModel(model="iasp91")
+        tt = model.get_travel_times((self.sacstats['evdp']),
+                                     self.sacstats['gcarc'],
+                                     [self.phase])
+   
+        traveltime = tt[0].time
+        evt_time = obspy.UTCDateTime(year = self.sacstats['nzyear'],
+                                     julday = self.sacstats['nzjday'],
+                                     hour=self.sacstats['nzhour'],
+                                     minute=self.sacstats['nzmin'],
+                                     second=self.sacstats['nzsec'],
+                                     microsecond=self.sacstats['nzmsec'])
+        tt_utc =  evt_time + traveltime
+        return tt_utc, traveltime
 
-    def gen_infile(self,path,label,phase,nwind=10,tlag_max=4.0):
-    
-        os.chdir(path) # Make sure we are in the right directory
-    
-        with open('sheba.in','w') as writer:
-            writer.write('SHEBA.IN \n')
-            writer.write('{}{} \n'.format(label,phase)) # write file prefix
-            for trace in self.st:
-                writer.write('{} \n'.format(trace.stats.channel)) # Write channels for each component (E, N, Z order)
-            writer.write('1 \n') # Specifies Eigenvalue minimisation, replace with spol if transverse minimisation is desired (not supported here)
-            writer.write('{:i} {:i} \n'.format(nwind,nwind))
-            writer.write('{} \n'.format(tlag_max)) # sets max tlag in gridsearch
-            writer.write('0')  
-            writer.write('0')
+    def initialise_windows(self, trace):
+        if all (k in trace.stats.sac for k in ('user0','user1','user2','user3')):
+            print('Pre-defined window ranges found')
+        else:
+            user0 = self.tt_rel - 15 # 15 seconds before arrival
+            user1 = self.tt_rel # t predicted arrival
+#           Set the range of window endtime (user2/user3)
+            user2 = self.tt_rel + 15 # 15 seconds after, gives a min window size of 20 seconds
+            user3 = self.tt_rel + 30 # 30 seconds after, gives a max window size of 45 seconds
+            keychain = {'user0':user0,'user1':user1,'user2':user2,'user3':user3}
+            trace.stats.sac.update(keychain)
 
+    def check_evdp(self, trace):
+        '''
+        Tests the event depths 
+        '''
+        evdp = trace.stats.sac.evdp
+        if trace.stats.sac.evdp >= 1000:
+            raise Warning('Event depth is greater than 1000km! EVDP may be in meters')
+            trace.stats.sac({'evdp':evdp/1000})
+        elif trace.stats.sac.evdp == 0:
+            raise ValueError('Event depth is 0km!')
 
-    def sheba(self,phase,label,path=None,nwind=True,):
+    def check_phase_dist(self,phase):
         """
-        The big one! This function uses the subprocess module to host sac and then runs sheba as a SAC macro
+        Function to test if the given phase is actually measureable!
         """
-        print(f'Worker {current_process()} Passing {label} into Sheba for.')
-        p = sub.Popen(['sac'],
-                     stdout = sub.PIPE,
-                     stdin  = sub.PIPE,
-                     stderr = sub.STDOUT,
-                     cwd = path,
-                     encoding='utf8')
-#       Now that the child process SAC has been opened, lets specify what arguements we want to pass to it
-#       echo on means commands should be echoed (so we can check whats going on if we print output)
-#       SETMACRO makes sure SAC can find sheba (avoids pathing problems)
-#       m sheba calls sheba as a SAC macro
-        if nwind == True:
-            s = '''
-            echo on\n
-            SETMACRO /Users/ja17375/Ext_programs/macros
-            m sheba file {}{} plot yes pick no nwind 10 10 batch yes
-            '''.format(label,phase)
-        elif nwind == False:
-            s = '''
-            echo on\n
-            SETMACRO /Users/ja17375/Ext_programs/macros
-            m sheba file {}{} plot yes pick yes batch yes
-            '''.format(label,phase)
-        try:           
-            _ = p.communicate(s)
-            # print(out[0])
-        except CalledProcessError as err:
-            print(err)
+        if phase == 'SKS':
+            if self.sacstats['gcarc'] > 145.0:
+                raise ValueError('Event-Station distance is greater than 145, SKS not visible/reliable.')
+        elif phase == 'SKKS':
+            if (self.sacstats['gcarc'] < 105.0) or (self.sacstats['gcarc'] > 145.0) :
+                raise ValueError('Event-Station distance outside of acceptable range of 105-145 for SKKS')
+        elif phase == 'ScS':
+            if self.sacstats['gcarc'] > 95.0:
+                raise ValueError('Event-Station distance greatr than acceptable distance of 95 deg for ScS')
+        else:
+            print(f'Phase {self.phase} not ScS, SKS or SKKS. Not checking!')
