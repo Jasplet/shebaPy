3d184e5f0be5e1e3b392a59495108f14c348ef03
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2021-08-23 14:32:07 +0100

    Updated example script

--- sheba_example.py
+++ sheba_example.py
@@ -8,11 +8,11 @@ Created on Wed Aug 18 16:45:38 2021
 Simple example (for one phase) of how to use the SHEBA wrapper
 """
 import obspy
-from wrapper import Interface
+from wrapper import Wrapper
 
 data = obspy.read('example/data/HKT_1995226_043721_SKS.BH?')
 
-Example = Interface(data, 'SKS', rundir='example/')
+Example = Wrapper(data, 'SKS', rundir='example/')
 # Preprocess method filters and trims data
 Example.preprocess()
-Example.measure_splitting('HKT_example', sheba_exec_path='/Users/ja17375/Ext_programs/bin', window=False)
+result = Example.measure_splitting('HKT_example', sheba_exec_path='/Users/ja17375/Ext_programs/bin', window=False)
