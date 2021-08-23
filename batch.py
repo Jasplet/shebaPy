d22b5222cd2a72138aea0006835c915679446c11
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2021-08-23 14:31:45 +0100

    new file for batch processing codes

---
+++ batch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 14:29:22 2021
+
+@author: ja17375
+
+batch.py enables batch processing of shear-wave splitting using a Python wrapper
+"""
+
