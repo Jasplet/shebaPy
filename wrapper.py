992a0b2e1e6d5d03b27345ef51142af7497d5cd8
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2021-08-27 12:46:10 +0100

    Some tidyups based on Spyder Code Analysis

--- wrapper.py
+++ wrapper.py
@@ -7,13 +7,12 @@ Created on Tue Aug 17 12:13:24 2021
 """
 
 import os
+import subprocess as sub
+from multiprocessing import current_process
+from netCDF4 import Dataset
 import obspy
 from obspy.taup import TauPyModel
 from windower import WindowPicker
-import subprocess as sub
-from multiprocessing import current_process
-from subprocess import CalledProcessError
-from netCDF4 import Dataset
 
 class Wrapper:
     """
@@ -21,34 +20,37 @@ class Wrapper:
     The "subprocess" sheba will be a bound method
     """
     def __init__(self,st, phase, rundir=None):
-        
+       
         self.st = st
         self.sacstats = st[0].stats.sac
-        self.station = st[0].stats.station # station code 
+        self.station = st[0].stats.station # station code
         self.delta = st[0].stats.delta # sample rate of seismometer [s]
-        # do some basic tests of the data 
+        # do some basic tests of the data
         self.check_phase_dist(phase)
         self.phase = phase # The shear-wave phase we are measuing splitting for!
         for trace in self.st:
             trace.stats.sac.kstnm = '{:>8}'.format(trace.stats.sac.kstnm)
-#          Formats Station name in headers so that it is 8 characters long, with emtpy character fill with whitespaces    
+#       Formats Station name in headers so that it is 8 characters long,
+#       with emtpy character fill with whitespaces
             self.check_evdp(trace)
-        
+
         self.tt_utc, self.tt_rel = self.model_traveltimes()
         if rundir is None:
             print('Setting rundir path to current working directory')
             self.path = os.getcwd()
         else:
             self.path = rundir
-    
+
     def preprocess(self,c1=0.01,c2=0.5):
         """
         Function to bandpass filter and trim the components
-        Seismograms are trimmed so that they start 1 minute before the expected arrival and end 2 minutes after the arrival
+        Seismograms are trimmed so that they start 1 minute before the expected arrival 
+        and end 2 minutes after the arrival
         c1 - [Hz] Lower corner frequency
-        c2 - [Hz] Upper corner frequency 
+        c2 - [Hz] Upper corner frequency
         By default traces will be filtered between 0.01Hz-0.5Hz.
-        Using an upper corner of 0.1Hz for SKS/SKKS is also common, but can cut out high f signal (but also reduces noise)
+        Using an upper corner of 0.1Hz for SKS/SKKS is also common, 
+        but can cut out high f signal (but also reduces noise)
         """
         for trace in self.st:
 #       De-mean and detrend each component
@@ -56,10 +58,10 @@ class Wrapper:
             trace.detrend(type='simple') #De-trends component
 #       Filter each component. Bandpass flag gives a bandpass-butterworth filter
             trace.filter("bandpass",freqmin= c1, freqmax= c2,corners=2,zerophase=True)
-#       Now trim each component to the input length
-#       Data is only trimmed if the record is longer than 3 minutes. 
-#       This is to ensure there is enough space for windowing, especially manual windowing. This record length makes sense
-#       for teleseismic SKS, SKKS, ScS data but may need revising for other shear-wave data.  
+#       Data is only trimmed if the record is longer than 3 minutes.
+#       This is to ensure there is enough space for windowing, especially manual windowing. 
+#       This record length makes sense for teleseismic SKS, SKKS, ScS data
+#       but may need revising for other shear-wave data.
             if (trace.stats.endtime - trace.stats.starttime) > 180.0:
                 print('Trim Traces')
                 t1 = (self.tt_utc - 60) #I.e A minute before the arrival
@@ -67,17 +69,18 @@ class Wrapper:
                 trace.trim(t1,t2)
             else:
                 print('Traces too short (<180 s ) to trim')
-#               Initialise Windows        
+#               Initialise Windows
             self.initialise_windows(trace)
 
     def measure_splitting(self,output_filename, sheba_exec_path, window=False, debug=False):
         """
         Measures Shear-wave splitting using Sheba. 
-        
+
         Args:
             output_filename (str): filestem to name processed data and SHEBA outfiles
             sheba_exec_path (str): path to the compiled SHEBA executable sheba_exec
-            window (bool | optional): switch to manual windowing (if True) or to use pre-defined windows (False)
+            window (bool | optional): switch to manual windowing (if True) 
+                                        or to use pre-defined windows (False)
             debug (bool | optional): prints out SHEBA stdout when True. Default is False
         """
         fname = f'{output_filename}'
@@ -86,26 +89,26 @@ class Wrapper:
                 self.window_event()
             except ValueError:
                 print('Event Skipped')
-                return 
-            
+                return
+
         self.gen_infile(fname)
         self.write_out(fname)
         print(f'Passing {fname} into Sheba.')
-        out = sub.run(f'{sheba_exec_path}/sheba_exec', capture_output=True, cwd=self.path)
+        out = sub.run(f'{sheba_exec_path}/sheba_exec', capture_output=True, cwd=self.path, check=True)
         if debug:
-            # print what sheba returns to stdout. useful for debugging the wrapping. 
+            # print what sheba returns to stdout. useful for debugging the wrapping.
             print(out)
         self.result = self.collate_result(fname)
-        # Maybe move all result netCDFs into one folder?  
-        
+        # Maybe move all result netCDFs into one folder?
+
     def collate_result(self, fname):
         '''Collate results after measurment into a Dict'''
-        
+
         raw_result = Dataset(f'{self.path}/{fname}_sheba_result.nc')
         print('Best fitting result is')
         print(f'Fast direction =  {raw_result.fast} +/- {raw_result.dfast}')
         print(f'Delay time = {raw_result.tlag} +/- {raw_result.dtlag}')
-    
+
         result = {'STAT':self.station, 'PHASE':self.phase,
                 'DATE':raw_result.zdate,'TIME':raw_result.ztime.split('.')[0],
                 'STLA':raw_result.stla, 'STLO':raw_result.stlo,
@@ -124,26 +127,28 @@ class Wrapper:
         '''
         with open(f'{self.path}/sheba.in','w') as writer:
             writer.write('SHEBA.IN \n')
-            writer.write(f'{filename} \n') # Name of pre-processed data 
+            writer.write(f'{filename} \n') # Name of pre-processed data
             for trace in self.st:
-                writer.write('{} \n'.format(trace.stats.channel)) # Write channels for each component (E, N, Z order)
-            writer.write('1 \n') # Specifies Eigenvalue minimisation, replace with spol if transverse minimisation is desired (not supported here)
+                writer.write('{} \n'.format(trace.stats.channel))
+            # 1 Specifies Eigenvalue minimisation, 
+            # replace with spol if transverse minimisation is desired (not supported here)
+            writer.write('1 \n')
             writer.write('{} {} \n'.format(nwind,nwind))
             writer.write('{} \n'.format(tlag_max)) # sets max tlag in gridsearch
-            writer.write('0 \n')  
+            writer.write('0 \n')
             writer.write('0')
-   
+
     def write_out(self,filename):
-      """
-      Function to write the component seismograms to SAC files to the SHEBA rundir.
-      The rundir is set by self.path.
-      
-      filename [str] - the output filename for each trace. 
-      
-      """
-      for trace in self.st:
-          ch = trace.stats.channel
-          trace.write(f'{self.path}/{filename}.{ch}', format='SAC', byteorder=1)
+        """
+        Function to write the component seismograms to SAC files to the SHEBA rundir.
+        The rundir is set by self.path.
+        
+        filename [str] - the output filename for each trace.
+        
+        """
+        for trace in self.st:
+            ch = trace.stats.channel
+            trace.write(f'{self.path}/{filename}.{ch}', format='SAC', byteorder=1)
 
     def window_event(self):
         '''Function that enables manual picking of window start/end ranges'''
@@ -160,11 +165,12 @@ class Wrapper:
                       'user2' : Windower.wend1, 'user3' : Windower.wend2}
             [trace.stats.sac.update(windows) for trace in self.st]
             return
-        
+
     def model_traveltimes(self):
         """
         Function to run TauP traveltime models for the SKS phase.
-        Returns SKS predictided arrivals (seconds), origin time of the event (t0) as a UTCDateTime obejct and the SKS arrival as a UTCDateTime object
+        Returns SKS predicted arrivals (seconds),
+        origin time of the event (t0) as a UTCDateTime obejct and the SKS arrival as a UTCDateTime object
         tr - trace object for which SKS arrival time will be predicted
         """
         model = TauPyModel(model="iasp91")
@@ -182,6 +188,7 @@ class Wrapper:
         return tt_utc, traveltime
 
     def initialise_windows(self, trace):
+        '''Add docstring '''
         if all (k in trace.stats.sac for k in ('user0','user1','user2','user3')):
             print('Pre-defined window ranges found')
         else:
