938101330244b553a2b95f946debccbb9e8ec676
Joseph Asplet <ja17375@it042614.users.bris.ac.uk>
2022-07-26 09:58:46 +0100

    Local changes to fix interactive windower (forgetting this fix had alreayd been kinda done)

--- windower.py
+++ windower.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from scipy.fftpack import fft
+from scipy.signal import hilbert
 
 # from .plots import plot_traces
 
@@ -33,7 +34,6 @@ class WindowPicker:
         None.
         '''
         #t0 = 60 seconds before traveltime (aka the start of the trimming seismogram)
-        
         self.st = st # Obspy stream containing BHN and BHE
         # st.plot()
         self.tt = tt
@@ -44,46 +44,51 @@ class WindowPicker:
         (self.wbeg1,self.wbeg2,self.wend1,self.wend2) = (wbeg1,wbeg2,wend1,wend2)
         (self.x1,self.x2,self.x3,self.x4) = (wbeg1,wbeg2,wend1,wend2)
         # Base plot (before interactive stuff)
-        fig = plt.figure(figsize = (18,8))
+        self.fig = plt.figure(figsize = (18,8))
         plt.suptitle(f'Station {st[0].stats.station}, Event Time {st[0].stats.starttime}')
         gs = gridspec.GridSpec(2,2)
-        self.ax1 = plt.subplot(gs[1,:]) # Bottom Row, for window picking
-        self.ax2 = plt.subplot(gs[0,:]) # Top Row, for fft plot
+        self.ax1 = plt.subplot(gs[0,:]) # Top Row, for fft plot
+        self.ax2 = plt.subplot(gs[1,:]) # Bottom Row, for window picking
         self.plot_fft()
-        self.canvas = fig.canvas
         # Add seismograms
-        self.ax1.plot(self.t, self.st[0].data,label=st[0].stats.channel, color='darkorange')
-        self.ax1.plot(self.t, self.st[1].data,label=st[1].stats.channel, color='dodgerblue')
-        self.ax1.set_xlabel('Time relative to origin (s)')
+        self.ax2.plot(self.t, self.st[0].data,label=st[0].stats.channel, color='darkorange')
+        self.ax2.plot(self.t, self.st[1].data,label=st[1].stats.channel, color='dodgerblue')
+        self.ax2.set_xlabel('Time relative to origin (s)')
+        # Add instantaneous amplitude envelopes to help pick out signal (should be envelope max at phase arrival)
+        ht1 = hilbert(self.st[0].data)
+        env1 = np.abs(ht1)
+        ht2 = hilbert(self.st[1].data)
+        env2 = np.abs(ht2)
+        self.ax2.plot(self.t, env1, color='darkorange',linestyle='--', label=None)
+        self.ax2.plot(self.t, env2, color='dodgerblue',linestyle='--', label=None)
         # Add legend
-        self.ax1.legend()
+        self.ax2.legend()
         # window limit lines
-        self.wbeg1line = self.ax1.axvline(self.wbeg1, linewidth=2, color='r', visible=True)
-        self.wbeg2line = self.ax1.axvline(self.wbeg2, linewidth=2, color='r', visible=True)
-        self.wend1line = self.ax1.axvline(self.wend1, linewidth=2, color='g', visible=True)
-        self.wend2line = self.ax1.axvline(self.wend2, linewidth=2, color='g', visible=True)
-        self.cursorline= self.ax1.axvline(100, linewidth=1, color='0.5', visible=False)
-        self.pred_tt= self.ax1.axvline(self.tt, linewidth=1, color='k', visible=True)
+        self.wbeg1line = self.ax2.axvline(self.wbeg1, linewidth=2, color='r', visible=True)
+        self.wbeg2line = self.ax2.axvline(self.wbeg2, linewidth=2, color='r', visible=True)
+        self.wend1line = self.ax2.axvline(self.wend1, linewidth=2, color='g', visible=True)
+        self.wend2line = self.ax2.axvline(self.wend2, linewidth=2, color='g', visible=True)
+        self.cursorline= self.ax2.axvline(100, linewidth=1, color='0.5', visible=False)
+        self.pred_tt= self.ax2.axvline(self.tt, linewidth=1, color='k', visible=True)
         _, self.ydat = self.wbeg1line.get_data()
         
         # set limits
         self.lim_max = max([self.st[0].data.max(), self.st[1].data.max()]) * 1.1
         self.lim_min = min([self.st[0].data.min(), self.st[1].data.min()]) * 1.1
         # self.ax1.set_aspect('equal')
-        self.ax1.set_ylim([self.lim_min,self.lim_max])
-        self.ax1.set_xlim(t0,max(self.t)) # Set ylim in relative time (from stsrt of stream )
+        self.ax2.set_ylim([self.lim_min,self.lim_max])
+        self.ax2.set_xlim(t0,max(self.t)) # Set ylim in relative time (from stsrt of stream )
         # Add some labels
-        self.phaselabel = self.ax1.text(self.tt + 1,
+        self.phaselabel = self.ax2.text(self.tt + 1,
                                         self.lim_max*0.8,"IASP91\nPred.\nArrival",
                                         multialignment='left')
-        self.wbeg1label = self.ax1.text(self.wbeg1 - 3, self.lim_min*0.85, 'S', color='r', fontsize=14)
-        self.wbeg2label = self.ax1.text(self.wbeg2 - 3, self.lim_min*0.85, 'F', color='r', fontsize=14)
-        self.wend1label = self.ax1.text(self.wend1 - 3, self.lim_min*0.85, 'S', color='g', fontsize=14)
-        self.wend2label = self.ax1.text(self.wend2 - 3, self.lim_min*0.85, 'F', color='g', fontsize=14)
+        self.wbeg1label = self.ax2.text(self.wbeg1 - 3, self.lim_min*0.85, 'S', color='r', fontsize=14)
+        self.wbeg2label = self.ax2.text(self.wbeg2 - 3, self.lim_min*0.85, 'F', color='r', fontsize=14)
+        self.wend1label = self.ax2.text(self.wend1 - 3, self.lim_min*0.85, 'S', color='g', fontsize=14)
+        self.wend2label = self.ax2.text(self.wend2 - 3, self.lim_min*0.85, 'F', color='g', fontsize=14)
         print("'a' & 'd' set the window beginnning range")
         print("'z' & 'c' set the window end range")
-        self.connect() # Dev only
-        plt.pause(1)
+        self.connect()
         plt.show()
 
     def plot_fft(self):
@@ -108,25 +113,24 @@ class WindowPicker:
         fft_h1 = fft(horz1.data)
         fft_h2 = fft(horz2.data)
         #Now plot the spectra
-        self.ax2.semilogy(xf[1:N//2], 2.0/N * np.abs(fft_h1[1:N//2]), color='darkorange')
-        self.ax2.semilogy(xf[1:N//2], 2.0/N * np.abs(fft_h2[1:N//2]), color='dodgerblue')
-        self.ax2.legend([st[0].stats.channel, st[1].stats.channel])
-        self.ax2.set_xlabel('Frequency [Hz]')
-        self.ax2.set_xlim([0.01, 0.5])
-        plt.grid()
-
+        self.ax1.semilogy(xf[1:N//2], 2.0/N * np.abs(fft_h1[1:N//2]), color='darkorange')
+        self.ax1.semilogy(xf[1:N//2], 2.0/N * np.abs(fft_h2[1:N//2]), color='dodgerblue')
+        self.ax1.legend([st[0].stats.channel, st[1].stats.channel])
+        self.ax1.set_xlabel('Frequency [Hz]')
+        self.ax1.set_xlim([0.01, 0.5])
+       
         # plt.show()
 
     def connect(self):
         '''
         Sets up interactive options in figure canvas
         '''
-        # self.cidclick = self.canvas.mpl_connect('button_press_event', self.click)
-        self.cidmotion = self.canvas.mpl_connect('motion_notify_event', self.motion)
+        # self.cidclick = self.fig.canvas.mpl_connect('button_press_event', self.click)
+        self.cidmotion = self.fig.canvas.mpl_connect('motion_notify_event', self.motion)
         # self.cidrelease = self.canvas.mpl_connect('button_release_event', self.release)
-        self.cidenter = self.canvas.mpl_connect('axes_enter_event', self.enter)
-        self.cidleave = self.canvas.mpl_connect('axes_leave_event', self.leave)
-        self.cidkey = self.canvas.mpl_connect('key_press_event', self.keypress)
+        self.cidenter = self.fig.canvas.mpl_connect('axes_enter_event', self.enter)
+        self.cidleave = self.fig.canvas.mpl_connect('axes_leave_event', self.leave)
+        self.cidkey = self.fig.canvas.mpl_connect('key_press_event', self.keypress)
 
     def keypress(self,event):
         ''' Define a set of keypress responses
@@ -139,67 +143,70 @@ class WindowPicker:
         if event.key == "a":
             print('WBEG Start')
             self.x1 = event.xdata
+            print(self.ydat)
             self.wbeg1line.set_data(self.x1,self.ydat)
             self.wbeg1label.set_position((self.x1 - 3, self.lim_min*0.85))
-            self.canvas.draw()
+            plt.draw()
             print(self.x1)
-        if event.key == "d":
+        elif event.key == "d":
             print('WBEG End')
             self.x2 = event.xdata
             self.wbeg2line.set_data(self.x2,self.ydat)
             self.wbeg2label.set_position((self.x2 - 3, self.lim_min*0.85))
-            self.canvas.draw()
+            plt.draw()
             print(self.x2)
-        if event.key == "z":
+        elif event.key == "z":
             print('WEND Start')
             self.x3 = event.xdata
             self.wend1line.set_data(self.x3,self.ydat)
             self.wend1label.set_position((self.x3 - 3, self.lim_min*0.85))
-            self.canvas.draw()
+            plt.draw()
             print(self.x3)
-        if event.key == "c":
+        elif event.key == "c":
             print('WEND End')
             self.x4 = event.xdata
             self.wend2line.set_data(self.x4,self.ydat)
             self.wend2label.set_position((self.x4 - 3, self.lim_min*0.85))
-            self.canvas.draw()
+            plt.draw()
             print(self.x4)
-        if event.key == "w":
+        elif event.key == "w":
             print('Bad/noisey waveform, will tell SHEBA to skip')
             self.x1 = False
             self.disconnect()
-        if event.key == "q":
+        elif event.key == "q":
             print('DISCONNECT')
             self.disconnect()
+        else:
+            print(f'Key {event.key} unknown')
 
     def enter(self,event):
         if event.inaxes is not self.ax2: return
         x = event.xdata
         self.cursorline.set_data([x,x],self.ydat)
         self.cursorline.set_visible(True)
-        self.canvas.draw()
+        plt.draw()
 
     def leave(self,event):
         if event.inaxes is not self.ax2: return
         self.cursorline.set_visible(False)
-        self.canvas.draw()
+        plt.draw()
 
     def motion(self,event):
         if event.inaxes is not self.ax2: return
         x = event.xdata
         self.cursorline.set_data([x,x],self.ydat)
-        self.canvas.draw()
+        plt.draw()
 
     def disconnect(self):
         '''
         disconnect all the stored connection ids
         '''
-        # self.canvas.mpl_disconnect(self.cidclick)
-        self.canvas.mpl_disconnect(self.cidmotion)
-        self.canvas.mpl_disconnect(self.cidenter)
-        self.canvas.mpl_disconnect(self.cidleave)
+        # self.fig.canvas.mpl_disconnect(self.cidclick)
+        self.fig.canvas.mpl_disconnect(self.cidmotion)
+        self.fig.canvas.mpl_disconnect(self.cidenter)
+        self.fig.canvas.mpl_disconnect(self.cidleave)
         if self.x1 == False:
-            'Bad waveform that we want to skip'
+            print('Bad waveform that we want to skip')
             self.wbeg1, self.wbeg2, self.wend1,self.wend2 = (None,None,None,None)
         else:
             self.wbeg1, self.wbeg2, self.wend1,self.wend2 = sorted((self.x1, self.x2,self.x3,self.x4))
